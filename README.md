c4decd041cdd6d5a9d62c28b31a4c0ca8aa49dfa
Joynal Bin Tofajjal <joynal.bintofajjal@gmail.com>
2022-11-05 11:41:06 +0600

    Read JSON file from FS and Sorting out by char

---
+++ README.md
@@ -0,0 +1,27 @@
+# আমার বাংলা - [Bin Tofajjal](https://bintofajjal.info/)
+
+### [ REQUIRED ] TERMINAL COMMAND
+
+#### Step: 1
+
+###### Install Node_Module
+
+```
+npm install
+```
+
+#### Step: 2
+
+###### Run in Your Local
+
+```
+yarn start:dev
+```
+
+#### Step: 3
+
+###### LocalHost: 8000
+
+```
+http://localhost:8000/?query=কলম
+```
---
+++ src/app.controller.ts
@@ -0,0 +1,64 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { AppService } from './app.service';
+
+export class Word {
+  word: string;
+  uccharon: string;
+  butpotti: string;
+  utsho: string;
+  porivasha: string;
+  bisessho: string;
+  bisheshon: string;
+  sorbonam: string;
+  obboy: string;
+  kriya: string;
+  kbisesso: string;
+  kbisheshon: string;
+  kmul: string;
+  extraortho: string;
+}
+
+@Controller()
+export class AppController {
+  private words = JSON.parse(
+    readFileSync('wordData/words.json', 'utf-8'),
+  ) as Word[];
+
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(@Query('query') query: string) {
+    console.log(query);
+
+    const matches: Word[] = [];
+
+    for (const word of this.words) {
+      if (matches.length === 10) {
+        break;
+      }
+
+      if (word.word.includes(query)) {
+        matches.push(word);
+      }
+    }
+
+    return matches.sort((a, b) => {
+      if (a.word.startsWith(query) && b.word.startsWith(query)) {
+        return 0;
+      }
+
+      if (a.word.startsWith(query) && !b.word.startsWith(query)) {
+        return -1;
+      }
+
+      if (!a.word.startsWith(query) && b.word.startsWith(query)) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    return matches;
+  }
+}
